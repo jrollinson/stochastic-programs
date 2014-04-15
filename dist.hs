f45e1e3c10256c11be8b4dc73fdd98473b66518c
Joseph Rollinson <JTRollinson@gmail.com>
2014-04-14 20:55:00 -0400

    Renamed vars in dist to strings

--- dist.hs
+++ dist.hs
@@ -6,26 +6,26 @@ import qualified Data.Set as Set
 
 expToString (v, exp) = show v ++ " := " ++ show exp
 
-network = Map.fromList [ (0, SFlip 0.01) -- earthquake
-                       , (1, SFlip 0.1)  -- burglary
-                       , (2, SFlip 0.99) -- a-when-e-b
-                       , (3, SFlip 0.2)  -- a-when-e-nb
-                       , (4, SFlip 0.98) -- a-when-ne-b
-                       , (5, SFlip 0.01) -- a-when-ne-nb
-                       , (6, SIf 1 2 3)  -- a-when-e
-                       , (7, SIf 1 4 5)  -- a-when-ne
-                       , (8, SIf 0 6 7)  -- alarm
+network = Map.fromList [ ("earthquake", SFlip 0.01)
+                       , ("burglary", SFlip 0.1)
+                       , ("a-when-e-b", SFlip 0.99)
+                       , ("a-when-e-nb", SFlip 0.2)
+                       , ("a-when-ne-b", SFlip 0.98)
+                       , ("a-when-ne-nb", SFlip 0.01)
+                       , ("a-when-e", SIf "burglary" "a-when-e-b" "a-when-e-nb")
+                       , ("a-when-ne", SIf "burglary" "a-when-ne-b" "a-when-ne-nb")
+                       , ("alarm", SIf "earthquake" "a-when-e" "a-when-ne")
                        ]
 
-result = 
-    let 
-      dist = pEval network 8 (Set.singleton 8)
+result =
+    let
+      d = dist network "alarm"
 
       netToString n = concatMap (\x -> expToString x ++ "\n") (Map.toList n)
 
-    in 
+    in
       concatMap (\(n, p) -> "P: " ++ show p ++ "\n" ++ netToString n ++ "\n")
-        dist
+        d
 
 
 main = do
